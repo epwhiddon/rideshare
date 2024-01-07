879ff0c24fb53c9b350815f0348ce1336040ce86
Pepper <epwhiddon@gmail.com>
2024-01-07 14:05:27 -0500

    init

---
+++ Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -0,0 +1,86 @@
+﻿namespace Mapbox.Examples
+{
+	using UnityEngine;
+	using Mapbox.Utils;
+	using Mapbox.Unity.Map;
+	using Mapbox.Unity.MeshGeneration.Factories;
+	using Mapbox.Unity.Utilities;
+	using System.Collections.Generic;
+
+	public class SpawnOnMap : MonoBehaviour
+	{
+		[SerializeField]
+		AbstractMap _map;
+
+		[SerializeField]
+		[Geocode]
+		string[] _locationStrings;
+		Vector2d[] _locations;
+
+		[SerializeField]
+        int numberOfLocations = 5; // Define the number of random locations you want
+
+        [SerializeField]
+        float latitudeRange = 10f; // Define the latitude range for randomization
+
+        [SerializeField]
+        float longitudeRange = 10f; // Define the longitude range for randomization
+
+		[SerializeField]
+		float _spawnScale = 100f;
+
+		[SerializeField]
+		GameObject _markerPrefab;
+
+		List<GameObject> _spawnedObjects;
+
+		void Start()
+		{
+			_locations = new Vector2d[_locationStrings.Length];
+			_spawnedObjects = new List<GameObject>();
+			for (int i = 0; i < _locationStrings.Length; i++)
+			{
+				var locationString = _locationStrings[i];
+				_locations[i] = Conversions.StringToLatLon(locationString);
+				var instance = Instantiate(_markerPrefab);
+				instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
+				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+				_spawnedObjects.Add(instance);
+			}
+		}
+
+		private void GenerateRandomLocations()
+        {
+            _locations = new Vector2d[numberOfLocations];
+            for (int i = 0; i < numberOfLocations; i++)
+            {
+                double randomLatitude = Random.Range(-latitudeRange, latitudeRange);
+                double randomLongitude = Random.Range(-longitudeRange, longitudeRange);
+                _locations[i] = new Vector2d(randomLatitude, randomLongitude);
+            }
+        }
+
+		 private void SpawnMarkers()
+        {
+            for (int i = 0; i < numberOfLocations; i++)
+            {
+                var instance = Instantiate(_markerPrefab);
+                instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
+                instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+                _spawnedObjects.Add(instance);
+            }
+        }
+
+		private void Update()
+		{
+			int count = _spawnedObjects.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var spawnedObject = _spawnedObjects[i];
+                var location = _locations[i];
+                spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+                spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+            }
+		}
+	}
+}
